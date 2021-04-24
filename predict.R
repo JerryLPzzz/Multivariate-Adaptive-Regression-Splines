c179fa96b25c872194336a798cc97dca101cbd00
JerryLPzzz <3423350278@qq.com>
2021-04-24 16:54:10 +0800

    functions for MARS
    check See Also in the documentation

---
+++ predict.R
@@ -0,0 +1,33 @@
+predict.mars <- function(object, newdata){
+  if(missing(newdata)|| is.null(newdata)){
+    return(object$fitted)
+  }
+  else{
+    tt <- terms(object$formula)
+    tt <- delete.response(tt)
+    mf <- model.frame(tt, newdata)
+    mt <- attr(mf, "terms")
+    X <- model.matrix(mt, mf)
+    X <- split_X(X, object$splits)
+    beta <- object$coefficients
+    return(drop(X %*% beta))
+  }
+}
+
+split_X <- function(X, splits){
+  Xout <- matrix(0, nrow=nrow(X), ncol=length(splits))
+  for(i in 1:length(splits)){
+    Xout[,i]<-one_split_X(X, splits[[i]])
+  }
+  return(Xout)
+}
+
+one_split_X <- function(X, split){
+  Xout <- rep(1, nrow(X))
+  if(nrow(split)>1){
+    for(i in 2:nrow(split)){
+      Xout <- Xout * h(X[,split[i,"v"]], split[i,"s"],split[i,"t"])
+    }
+  }
+  return(Xout)
+}
